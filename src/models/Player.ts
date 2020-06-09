9e0369b9ebe7be6dee42af48dbd906bf2a754ee7
Zackery Mowrer <Zackmowrer@gmail.com>
2020-06-09 12:22:21 -0400

    Add new Player interface

---
+++ src/models/Player.ts
@@ -0,0 +1,13 @@
+// Import
+import { PlayerMark, Grid } from './Board';
+import Coordinate from './Coordinate';
+
+// Interface
+interface Player {
+    mark: PlayerMark;
+
+    selectMove(grid: Grid): Coordinate;
+}
+
+// Export
+export default Player;
