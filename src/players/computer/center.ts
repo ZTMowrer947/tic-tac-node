52d9e93b5cbdf7af123527c8db8d25bf70d570c4
Zackery Mowrer <Zackmowrer@gmail.com>
2020-06-09 09:45:09 -0400

    Add center move finder

---
+++ src/players/computer/center.ts
@@ -0,0 +1,12 @@
+// Imports
+import { Grid } from '../../models/Board';
+import Coordinate from '../../models/Coordinate';
+
+// Functions
+function findCenterMove(grid: Grid): Coordinate | null {
+    // Play center if it is not already taken
+    return grid[1][1] === ' ' ? { x: 1, y: 1 } : null;
+}
+
+// Exports
+export default findCenterMove;
